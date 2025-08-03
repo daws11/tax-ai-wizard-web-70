b14b66e7c93da996d766ee74b7bf985bc694b18e
daws11 <dawskutel@gmail.com>
2025-08-03 09:15:05 +0700

    chore: update API URL for production environment
    - Changed API_URL in the environment configuration to point to the production backend URL on Render.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const config = {
-  API_URL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  API_URL: import.meta.env.VITE_API_URL || 'https://tax-ai-backend-dm7p.onrender.com/',
   STRIPE_PUBLISHABLE_KEY: import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY || 'pk_test_51RcyvKQx4EP1ovnXxbzK5N2h13FK3j7nf3ta4zxq7Sg1URcYS3HKkh79qS8qmoJvez5T08zMGAOLMGjP1DXiBF1K00oSf9T3QL',
   DASHBOARD_URL: 'https://dashboard.taxai.ae/',
 }; 
